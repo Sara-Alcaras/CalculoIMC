0a3c4f230bc7050ffe6bfb6ec20962c0ffd6c2f7
Sara-Alcaras <saraalcaras401@gmail.com>
2022-06-14 20:16:09 -0300

    Cria metodo inicial do programa e valida o nome

--- Program.cs
+++ Program.cs
@@ -2,34 +2,104 @@
 
 public class Program
 {
+    #region Propriedade pública
+    public static string Nome { get; set; }
+    public static string Sexo { get; set; }
+    public static float Idade { get; set; }
+    public static float Altura { get; set; }
+    public static float Peso { get; set; }
+    public static float ImcDesejavel { get; set; }
+    public static float ImcRisco { get; set; }
+    public static float ImcRecomendacao { get; set; }
+    #endregion
     static void Main(string[] args)
     {
-        float imc, altura, peso;
+        #region Chama os métodos
+        //Método com entrada de dados
+        Inicio();
 
-        Console.WriteLine("Calculo IMC");
+        //Método que faz o calculo do IMC
+        //calculaIMC();
+        #endregion
 
-        Console.WriteLine("Insira o peso(kg): ");
-        peso = float.Parse(Console.ReadLine());
-
-        Console.WriteLine("Insira a altura: ");
-        altura = float.Parse(Console.ReadLine());
-
-        imc = peso / (altura * altura);
-        Console.WriteLine("Seu IMC é: " + imc.ToString("F2"));
-
-        if (imc < 18.5)
-            Console.WriteLine("Magreza");
-        else if (imc >= 18.5 && imc < 24.9)
-            Console.WriteLine("Normal");
-        else if (imc >= 24.9 && imc < 30)
-            Console.WriteLine("Sobrepeso");
-        else if (imc >= 30 && imc < 35)
-            Console.WriteLine("Obesidade grau 1");
-        else if (imc >= 35 && imc < 40)
-            Console.WriteLine("Obesidade grau 2");
-        else
-            Console.WriteLine("Obesidade grau 3");
     }
+
+    public static void Inicio()
+    {
+        //deve conter todos atributos relacionados a entrada de dados
+        //criar metodo de iniciliazação - output de mensagens
+        Console.WriteLine("Olá! Seja bem-vindo. Insira seus dados para calcular o DIAGNÓSTICO PRÉVIO");
+        Console.WriteLine("*************************************************************************");
+
+        Console.Write("\rInforme seu nome: ");
+        Nome = ValidaNome(Console.ReadLine());
+
+        Console.Write("Informe seu sexo: ");
+        Sexo = Console.ReadLine();
+
+        Console.Write("Informe sua idade: ");
+        Idade = float.Parse(Console.ReadLine());
+
+        Console.Write("Insira a altura: ");
+        Altura = float.Parse(Console.ReadLine());
+
+        Console.Write("Insira o peso(kg): ");
+        Peso = float.Parse(Console.ReadLine());
+
+
+    }
+
+    //criar um metodo de FIM
+    //DEVE retornar todos os atributos relacionados a saida de dados
+
+
+    //criar Metodo calculaIMC
+    //public static void calculaIMC()
+    //{
+    //    imc = Peso / (Altura * Altura);
+    //    Console.WriteLine("Seu IMC é: " + imc.ToString());
+    //}
+
+
+    //criar um metodo verifica categoria  
+
+
+    //criar Metodo Verifica imc baseado na propriedade imc publica
+    // o metodo deve utilizar o
+    // switch (imc) {
+    //      case < 20:
+    //          ImcRisco = escrever mensagem do risco;
+    //ImcDesejavel = escrever mensagem do imc desejavel;
+    //ImcRecomendacao = escrever mensagem da recomendacaoinicial
+    //      break;
+    // default: break;
+    // }
+
+
+    public static string ValidaNome(string nome) // Verifica se o nome é valido
+    {
+        if (nome == null || nome == " " || nome.Length <= 1)
+        {
+            Console.Write("Nome Inválido! \r\nInforme seu nome novamente: ");
+            ValidaNome(Console.ReadLine());
+        }
+        return nome;
+    }
+
+
+    //     if (imc< 20)
+    //            Console.WriteLine("Abaixo do peso ideal: Muitas complicações de saúde como doenças pulmonares e cardiovasculares podem estar associadas ao baixo peso.");
+
+    //        else if (imc >= 20 && imc< 24)
+    //            Console.WriteLine("Peso normal: Seu peso está ideal para suas referências.");
+
+    //        else if (imc >= 24 && imc< 30)
+    //            Console.WriteLine("Excesso de peso: Aumento de peso apresenta risco moderado para outras doenças crônicas e cardiovasculares.");
+
+    //        else if (imc >= 30 && imc< 35)
+    //            Console.WriteLine("Obesidade: Quem tem obesidade vai estar mais exposto a doenças graves e ao risco de mortalidade.");
+
+    //        else if (imc >= 35)
+    //            Console.WriteLine("Super obesidade: obeso mórbido vive menos, tem alto risco de mortalidade geral por diversas causas. ");
+    //
 }
-
-
