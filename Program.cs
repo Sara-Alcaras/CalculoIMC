7b8d1146b080bed2b6ebe09f8952b5d6da1c9170
Sara-Alcaras <saraalcaras401@gmail.com>
2022-06-14 22:23:39 -0300

    cria metodo para validar todas as entradas de dados

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ public class Program
 {
     #region Propriedade pública
     public static string Nome { get; set; }
-    public static string Sexo { get; set; }
-    public static float Idade { get; set; }
+    public static int Sexo { get; set; }
+    public static int Idade { get; set; }
     public static float Altura { get; set; }
     public static float Peso { get; set; }
-    public static float ImcDesejavel { get; set; }
+    public static string ImcDesejavel { get; set; }
     public static float ImcRisco { get; set; }
-    public static float ImcRecomendacao { get; set; }
+    public static string ImcRecomendacao { get; set; }
     #endregion
     static void Main(string[] args)
     {
@@ -24,31 +24,90 @@ public class Program
 
     }
 
+    #region Método que contém todos os atributos para entrada de dados
     public static void Inicio()
     {
-        //deve conter todos atributos relacionados a entrada de dados
-        //criar metodo de iniciliazação - output de mensagens
-        Console.WriteLine("Olá! Seja bem-vindo. Insira seus dados para calcular o DIAGNÓSTICO PRÉVIO");
+        Console.WriteLine("Olá! Seja bem-vindo. Insira seus dados para calcular o DIAGNÓSTICO PRÉVIO IMC");
         Console.WriteLine("*************************************************************************");
 
-        Console.Write("\rInforme seu nome: ");
+        Console.Write("\nInforme seu nome: ");
         Nome = ValidaNome(Console.ReadLine());
 
-        Console.Write("Informe seu sexo: ");
-        Sexo = Console.ReadLine();
+        Console.WriteLine("\nDigite o número que corresponde ao seu sexo: \n1- Masculino\n2- Feminino\n3- Outro ");
+        Sexo = ValidaSexo(Console.ReadLine());
 
-        Console.Write("Informe sua idade: ");
-        Idade = float.Parse(Console.ReadLine());
+        Console.Write("\nInforme sua idade: ");
+        Idade = ValidaIdade(int.Parse(Console.ReadLine()));
 
-        Console.Write("Insira a altura: ");
+        Console.Write("\nInforme sua altura em centímetros(cm): ");
         Altura = float.Parse(Console.ReadLine());
 
-        Console.Write("Insira o peso(kg): ");
+        Console.Write("\nInforme seu peso(kg): ");
         Peso = float.Parse(Console.ReadLine());
 
+        Console.Clear();
+    }
+    #endregion
 
+    #region Métodos para validação de entrada de dados
+    public static string ValidaNome(string nome) // Verifica se o nome é válido
+    {
+        if (nome == null || nome == " " || nome.Length <= 1)
+        {
+            Console.Write("\nNome Inválido! \r\nInforme seu nome novamente: ");
+            ValidaNome(Console.ReadLine());
+        }
+        return nome;
     }
 
+    public static int ValidaSexo(string strSexo) // Verifica se o sexo é válido
+    {
+        int sexo = int.Parse(strSexo);
+
+        switch (sexo)
+        {
+            case 1 or 2 or 3:
+                break;
+            default:
+                Console.Write("\nSexo inválido! \r\nInforme uma opção válida:  ");
+                ValidaSexo(Console.ReadLine());
+                break;
+        }
+        return sexo;
+    }
+
+    public static int ValidaIdade(int idade) // Verifica se a idade é válida
+    {
+        if (idade <= 0 || idade == 100)
+        {
+            Console.Write("\nIdade Inválida! \r\nInforme sua idade novamente: ");
+            ValidaIdade(int.Parse(Console.ReadLine()));
+        }
+        return idade;
+    }
+
+    public static float ValidaAltura(float altura) // Verifica se a altura é válida
+    {
+
+        if (altura <= 0)
+        {
+            Console.Write("\nIdade Inválida! \r\nInforme sua idade novamente: ");
+            ValidaIdade(int.Parse(Console.ReadLine()));
+        }
+        return altura;
+    }
+
+    public static float ValidaPeso(float peso)
+    {
+
+        if (peso <= 0)
+        {
+            Console.Write("\nIdade Inválida! \r\nInforme sua idade novamente: ");
+            ValidaIdade(int.Parse(Console.ReadLine()));
+        }
+        return peso;
+    }
+    #endregion
     //criar um metodo de FIM
     //DEVE retornar todos os atributos relacionados a saida de dados
 
@@ -57,7 +116,7 @@ public class Program
     //public static void calculaIMC()
     //{
     //    imc = Peso / (Altura * Altura);
-    //    Console.WriteLine("Seu IMC é: " + imc.ToString());
+    //    Console.WriteLine("Resultado IMC: " + imc.ToString());
     //}
 
 
@@ -74,18 +133,6 @@ public class Program
     //      break;
     // default: break;
     // }
-
-
-    public static string ValidaNome(string nome) // Verifica se o nome é valido
-    {
-        if (nome == null || nome == " " || nome.Length <= 1)
-        {
-            Console.Write("Nome Inválido! \r\nInforme seu nome novamente: ");
-            ValidaNome(Console.ReadLine());
-        }
-        return nome;
-    }
-
 
     //     if (imc< 20)
     //            Console.WriteLine("Abaixo do peso ideal: Muitas complicações de saúde como doenças pulmonares e cardiovasculares podem estar associadas ao baixo peso.");
