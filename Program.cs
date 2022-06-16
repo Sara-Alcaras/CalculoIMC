610f4c91f5fd7562ae5a59279f6dddb2363f721a
Sara-Alcaras <saraalcaras401@gmail.com>
2022-06-16 00:19:10 -0300

    Adiciona metodo fim, verifica dados, verifica categoria e verifica imc

--- Program.cs
+++ Program.cs
@@ -4,30 +4,38 @@ public class Program
 {
     #region Propriedade pública
     public static string Nome { get; set; }
-    public static int Sexo { get; set; }
+    public static string Sexo { get; set; }
     public static int Idade { get; set; }
     public static float Altura { get; set; }
     public static float Peso { get; set; }
-    public static string ImcDesejavel { get; set; }
+    public static float Imc { get; set; }
+    public static string Categoria { get; set; }
     public static string ImcRisco { get; set; }
     public static string ImcRecomendacao { get; set; }
-    //public static System.Globalization.CultureInfo InvariantCulture { get; }
     #endregion
     static void Main(string[] args)
     {
         #region Chama os métodos
         //Método com entrada de dados
         Inicio();
-
+        VerificaDados();
+        Fim();
         #endregion
 
+    }
+
+    private static void VerificaDados()
+    {
+        CalculaIMC(Peso, Altura);
+        VerificaCategoria(Idade);
+        VerificaIMC(Imc);
     }
 
     #region Método que contém todos os atributos para entrada de dados
     public static void Inicio()
     {
         Console.WriteLine("Olá! Seja bem-vindo. Insira seus dados para calcular o DIAGNÓSTICO PRÉVIO IMC");
-        Console.WriteLine("*************************************************************************");
+        Console.WriteLine("*****************************************************************************");
 
         Console.Write("\nInforme seu nome: ");
         Nome = ValidaNome(Console.ReadLine());
@@ -61,14 +69,22 @@ public class Program
         return nome;
     }
 
-    public static int ValidaSexo(string strSexo) // Verifica se o sexo é válido
+    public static string ValidaSexo(string strSexo) // Verifica se o sexo é válido
     {
-        int sexo;
-        int.TryParse(strSexo, out sexo);
+        string sexo = string.Empty;
+        int tipoSexo;
+        int.TryParse(strSexo, out tipoSexo);
 
-        switch (sexo)
+        switch (tipoSexo)
         {
-            case 1 or 2 or 3:
+            case 1:
+                sexo = "Masculino";
+                break;
+            case  2 :
+                sexo = "Feminino";
+                break;
+            case 3:
+                sexo = "Outro";
                 break;
             default:
                 Console.Write("\nSexo inválido! \r\nInforme uma opção válida:  ");
@@ -95,27 +111,27 @@ public class Program
         return idade;
     }
 
-    public static int ValidaAltura(string strAltura) // Verifica se a altura é válida
+    public static float ValidaAltura(string strAltura) // Verifica se a altura é válida
     {
-        int altura;
-        int.TryParse(strAltura, out altura);
+        float altura;
+        float.TryParse(strAltura, out altura);
 
         switch (altura)
         {
             case >= 100 and <= 200:
                 break;
             default:
-                Console.Write("\nIdade Inválida! \r\nInforme sua altura novamente: ");
+                Console.Write("\nAltura Inválida! \r\nInforme sua altura novamente: ");
                 ValidaAltura(Console.ReadLine());
                 break;
         }
         return altura;
     }
 
-    public static int ValidaPeso(string strPeso) // Verifica se o peso é válido
+    public static float ValidaPeso(string strPeso) // Verifica se o peso é válido
     {
-        int peso;
-        int.TryParse(strPeso, out peso);
+        float peso;
+        float.TryParse(strPeso, out peso);
 
         switch (peso)
         {
@@ -129,59 +145,72 @@ public class Program
         return peso;
     }
     #endregion
-    public static string VerificaCategoria(int idade) // Define a categoria de idade
+    public static void VerificaCategoria(int idade) // Define a categoria de idade
     {
-        if (idade < 12) return "Infantil";
-        if (idade >= 12 && idade <= 20) return "Juvenil";
-        if (idade >= 21 && idade <= 65) return "Adulto";
-        if (idade > 65) return "Idoso";
-        return "Nenhuma";
+        switch (idade)
+        {
+            case < 12:
+               Categoria = "Infantil";
+                break;
+            case >= 12 and <= 20:
+                Categoria = "Juvenil";
+                break;
+            case >= 21 and <= 65:
+                Categoria = "Adulto";
+                break;
+            case > 65:
+                Categoria = "Idoso";
+                break;
+                
+        }
     }
-    public static float calculaIMC(float peso, float altura) // Método que faz o calculo do IMC
+    public static void CalculaIMC(float peso, float altura) // Método que faz o calculo do IMC
     {
-
-        float imc = peso / (altura * altura);
-        return imc;
+        Imc = peso / (altura * altura);
+        
     }
 
-    //criar Metodo Verifica imc baseado na propriedade imc publica
+    public static void VerificaIMC(float imc) // Método que faz o calculo do IMC
+    {
+        switch (imc)
+        {
+            case < 20:
+                ImcRecomendacao = "Inclua carboidratos simples em sua dieta, além de proteínas - indispensáveis para ganho de massa magra. Procure um profissional";
+                ImcRisco = "Muitas complicações de saúde como doenças pulmonares e cardiovasculares podem estar associadas ao baixo peso.";
+                break;
+            case >= 20 and <= 24:
+                ImcRecomendacao = "Mantenha uma dieta saudável e faça seus exames periódicos.";
+                ImcRisco = "Seu peso está ideal para suas referências.";
+                break;
+            case >= 25 and <= 29:
+                ImcRecomendacao  = "Adote um tratamento baseado em dieta balanceada, exercício físico e medicação. A ajuda de um profissional pode ser interessante";
+                ImcRisco= "Aumento de peso apresenta risco moderado para outras doenças crônicas e cardiovasculares.";
+                break;
+            case >= 30 and <= 35:
+                ImcRecomendacao = "Adote uma dieta alimentar rigorosa, com o acompanhamento de um nutricionista e um médico especialista(endócrino).";
+                ImcRisco = "Quem tem obesidade vai estar mais exposto a doenças graves e ao risco de mortalidade.";
+                break;
+            case > 35:
+                ImcRecomendacao = "Procure com urgência o acompanhamento de um nutricionista para realizar reeducação alimentar, um psicólogo e um médico especialista(endócrino).";
+                ImcRisco = "O obeso mórbido vive menos, tem alto risco de mortalidade geral por diversas causas.";
+                break;
+        }
+    }
+    public static void Fim()
+    {
+        Console.WriteLine("Resultado DIAGNÓSTICO PRÉVIO IMC");
+        Console.WriteLine("********************************");
 
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Sexo: {Sexo}");
+        Console.WriteLine($"Idade: {Idade}");
+        Console.WriteLine($"Altura: {Altura}");
+        Console.WriteLine($"Peso: {Peso}");
+        Console.WriteLine($"Categoria: {Categoria}");
+        Console.WriteLine($"\n\nIMC Desejável: Entre 20 e 24");
 
-
-    // o metodo deve utilizar o
-
-
-    //     if (imc< 20)
-    //            Console.WriteLine("Abaixo do peso ideal: Muitas complicações de saúde como doenças pulmonares e cardiovasculares podem estar associadas ao baixo peso.");
-
-    //        else if (imc >= 20 && imc< 24)
-    //            Console.WriteLine("Peso normal: Seu peso está ideal para suas referências.");
-
-    //        else if (imc >= 24 && imc< 30)
-    //            Console.WriteLine("Excesso de peso: Aumento de peso apresenta risco moderado para outras doenças crônicas e cardiovasculares.");
-
-    //        else if (imc >= 30 && imc< 35)
-    //            Console.WriteLine("Obesidade: Quem tem obesidade vai estar mais exposto a doenças graves e ao risco de mortalidade.");
-
-    //        else if (imc >= 35)
-    //            Console.WriteLine("Super obesidade: obeso mórbido vive menos, tem alto risco de mortalidade geral por diversas causas. ");
-    //
-
-    //criar um metodo de FIM
-    //DEVE retornar todos os atributos relacionados a saida de dados
-    //public static void Fim()
-    //{
-    //    Console.WriteLine("Resultado DIAGNÓSTICO PRÉVIO IMC");
-    //    Console.WriteLine("*************************************************************************");
-
-    //    Console.WriteLine($"Nome: {Nome}");
-    //    Console.WriteLine($"Sexo: {Sexo}");
-    //    Console.WriteLine($"Idade: {Idade}");
-    //    Console.WriteLine($"Altura: {Altura}");
-    //    Console.WriteLine($"Peso: {Peso}");
-    //    Console.WriteLine($"Categoria: {VerificaCategoria(Idade)} \r\n");
-    //    Console.WriteLine($"IMC Desejável: Entre 20 e 24");
-
-    //    var imc = calculaIMC(altura, peso);
-    //}
+        Console.WriteLine($"\n\rResultado IMC: {Imc.ToString("F2")}");
+        Console.WriteLine($"\n\rRiscos: {ImcRisco}");
+        Console.WriteLine($"\n\rRecomendações: {ImcRecomendacao}");
+    }
 }
